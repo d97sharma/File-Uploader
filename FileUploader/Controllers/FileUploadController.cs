e9089f7d833cd156c32e44cf6c1d754484133111
d97sharma <disharma@firstam.com>
2019-10-16 16:32:46 +0530

    File Uploader

---
+++ FileUploader/Controllers/FileUploadController.cs
@@ -0,0 +1,75 @@
+﻿using FileUploader.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace FileUploader.Controllers
+{
+    [RoutePrefix("api/upload")]
+    public class FileUploadController : ApiController
+    {
+
+        [System.Runtime.InteropServices.ComVisible(true)]
+        
+        [Route("files")]
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    //var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
+                    //var filePath = "C:\\Tech Talk\\FileUploader\\FileUploader\\FileUploader\\Uploaded" + "~/" + postedFile.FileName;
+                    var filePath = "C:\\Uploaded Files\\" + postedFile.FileName;
+                    postedFile.SaveAs(filePath);
+                    docfiles.Add(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+
+
+
+
+        //// GET: FileUpload
+        //private static readonly string ServerUploadFolder = "C:\\Temp"; //Path.GetTempPath();
+
+        //[Route("files")]
+        //[HttpPost]
+        //[ValidateMimeMultipartContentFilter]
+
+        //public async Task<FileResult> UploadSingleFile()
+        //{
+        //    var streamProvider = new MultipartFormDataStreamProvider(ServerUploadFolder);
+        //    await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+        //    return new FileResult
+        //    {
+        //        FileNames = streamProvider.FileData.Select(entry => entry.LocalFileName),
+        //        Names = streamProvider.FileData.Select(entry => entry.Headers.ContentDisposition.FileName),
+        //        ContentTypes = streamProvider.FileData.Select(entry => entry.Headers.ContentType.MediaType),
+        //        Description = streamProvider.FormData["description"],
+        //        CreatedTimestamp = DateTime.UtcNow,
+        //        UpdatedTimestamp = DateTime.UtcNow,
+        //        DownloadLink = "TODO, will implement when file is persisited"
+        //    };
+        //}
+    }
+}
---
+++ FileUploader/Controllers/UploadController.cs
@@ -0,0 +1,58 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Net.Http.Headers;
+//using System.Threading.Tasks;
+//using System.Web.Hosting;
+//using System.Web.Mvc;
+////using Microsoft.AspNetCore.Hosting;
+////using Microsoft.AspNetCore.Mvc;
+
+//namespace FileUploader.Controllers
+//{
+//    public class UploadController : Controller
+//    {
+//        // GET: Upload
+//        private HostingEnvironment _hostingEnvironment;
+
+//        public UploadController(HostingEnvironment hostingEnvironment)
+//        {
+//            _hostingEnvironment = hostingEnvironment;
+//        }
+
+//        [HttpPost]//, DisableRequestSizeLimit]
+//        public ActionResult UploadFile()
+//        {
+//            try
+//            {
+//                foreach (var file in Request.Form.Files)
+//                {
+//                    string folderName = "Upload";
+//                    string webRootPath = _hostingEnvironment.WebRootPath;
+//                    string newPath = Path.Combine(webRootPath, folderName);
+//                    if (!Directory.Exists(newPath))
+//                    {
+//                        Directory.CreateDirectory(newPath);
+//                    }
+//                    if (file.Length > 0)
+//                    {
+//                        string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+//                        string fullPath = Path.Combine(newPath, fileName);
+//                        using (var stream = new FileStream(fullPath, FileMode.Create))
+//                        {
+//                            file.CopyTo(stream);
+//                        }
+//                    }
+//                }
+
+//                return Json("Upload Successful.");
+//            }
+//            catch (System.Exception ex)
+//            {
+//                return Json("Upload Failed: " + ex.Message);
+//            }
+//        }
+
+//    }
+//}
